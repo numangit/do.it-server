dd44da7b993d63323d1888472c3d094424dfa325
numangit <abdulnumanjob@gmail.com>
2022-12-29 09:30:59 +0600

    CRUD end points has been added

--- index.js
+++ index.js
@@ -17,6 +17,71 @@ async function run() {
     try {
         //database collections
         const tasksCollection = client.db('doit').collection('tasks');
+
+        //api to get tasks by user email
+        app.get('/myTasks', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = tasksCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        });
+
+        //api to get specific task by id
+        app.get('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const task = await tasksCollection.findOne(query);
+            res.send(task);
+        });
+
+        //api to add tasks data 
+        app.post('/myTasks', async (req, res) => {
+            const task = req.body;
+            const result = await tasksCollection.insertOne(task);
+            res.send(result);
+        });
+
+        //api to delete task
+        app.delete('/myTasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await tasksCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        //api to add the completed field on task
+        app.put('/myTasks/completed/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    completed: true
+                }
+            }
+            const result = await tasksCollection.updateOne(query, updatedDoc, options);
+            res.send(result);
+        })
+
+        //api to update the task
+        app.patch('/myTasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const taskDescription = req.body.taskDescription
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    taskDescription: taskDescription
+                }
+            }
+            const result = await tasksCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
     }
     finally {
     }
